9ace4bcba68db72204c4a64deb1d252ee9fb746e
Jose María Martín Celada <josemc90@gmail.com>
2015-02-03 21:48:55 +0100

    Ejercicio 5.5.6 terminado

---
+++ calculator.js
@@ -0,0 +1,15 @@
+
+function adder(){
+        var vari = document.getElementById("op").innerHTML;
+        var num = vari.split("+");
+        var a = parseInt(num[0]);
+        var b = parseInt(num[1]);
+        var c = a + b;
+        var res = document.getElementById("res");
+        res.innerHTML=" = "+c.toString();
+    };
+
+function random_adder(){
+        var vari = document.getElementById("op");
+        vari.innerHTML=(Math.floor(Math.random()*1000)+"+"+Math.floor(Math.random()*1000));
+}
